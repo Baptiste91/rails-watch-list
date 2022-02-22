fb8fe781e862fd0cf8ce7b9c6d072ef3d512539d
baptiste dauphin <bap_dauphin@hotmail.fr>
2022-02-22 10:01:46 +0100

    added a movies page and search bar

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,9 @@
 class MoviesController < ApplicationController
+  def index
+    if params[:query].present?
+      @moviess = Movie.where("name ILIKE ?","%#{params[:query]}%")
+    else
+      @movies = Movie.all
+    end
+  end
 end
