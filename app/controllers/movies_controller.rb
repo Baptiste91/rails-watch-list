83cf2faecfcb3b8ed7e2d65357981497260ebaf0
baptiste dauphin <bap_dauphin@hotmail.fr>
2022-05-11 13:29:54 +0200

    new version of ruby

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,7 @@
 class MoviesController < ApplicationController
   def index
     if params[:query].present?
-      @moviess = Movie.where("name ILIKE ?","%#{params[:query]}%")
+      @movies = Movie.where("title ILIKE ?" , "%#{params[:query]}%")
     else
       @movies = Movie.all
     end
