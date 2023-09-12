48674527ea7f3c52537deef470c60b7528eaa164
baptiste dauphin <bap_dauphin@hotmail.fr>
2023-09-12 12:39:23 +0200

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'lists#index'
+  get "home", to: "pages#home"
   resources :lists, only: [:show, :index, :new, :create, :destroy] do
     resources :bookmarks, only: [:new, :create]
   end
